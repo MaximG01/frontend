b5b907970034c3ee2af3993c59e2f661a6f3310b
MaximG01 <102536881+MaximG01@users.noreply.github.com>
2022-12-26 23:04:49 -0500

    Totally forgot to keep up with commits on frontend

---
+++ src/Component/UserComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Urls from '../Urls';
+
+class UserComponent extends React.Component{
+  /*  this.state = {
+        user:[]
+    }*/
+}
+export default UserComponent;
